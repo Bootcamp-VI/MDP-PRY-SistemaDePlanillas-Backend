65c441919e0816147b75ee1d48ef9495e7348663
Fcampos05 <0512018034@alumnos.unp.edu.pe>
2023-06-22 22:01:29 -0500

    first commit

---
+++ src/main/java/com/mdp/securityservice/MdpPrySistemaDePlanillasSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.mdp.securityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdpPrySistemaDePlanillasSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdpPrySistemaDePlanillasSecurityApplication.class, args);
+	}
+
+}
